757fe396044ea0e6807477b66a91169fea60037f
Attack on Titan <ansu.yovan@gmail.com>
2021-01-16 13:48:39 +0530

    angry birds

--- sketch.js
+++ sketch.js
@@ -3,11 +3,14 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var engine, world;
-var box1, pig1,pig3;
-var backgroundImg,platform;
-var bird, slingshot;
-
+var engine, world; 
+var box1, box2,box3,box4,box5; 
+var pig1,pig2; 
+var log1,log2,log3,log4,log5; 
+var bird;
+var backgroundImg;
+var slingshot;
+var gameState = "onSling";
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -31,11 +34,11 @@ function setup(){
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
 
-    log3 =  new Log(810,180,300, PI/2);
+    log2 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    log3 = new Log(760,120,150, PI/7);
+    log4 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(200,50);
 
@@ -56,11 +59,11 @@ function draw(){
     box3.display();
     box4.display();
     pig3.display();
-    log3.display();
+    log2.display();
 
     box5.display();
+    log3.display();
     log4.display();
-    log5.display();
 
     bird.display();
     platform.display();
@@ -69,16 +72,20 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+    
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched";
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+ //       slingshot.attach(bird.body);
     }
 }
